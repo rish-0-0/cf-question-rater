68996323bc08c22995f0c7fbbc0203f2b6a33021
Rishabh Anand <rishabhanand42@gmail.com>
2020-07-02 16:34:04 +0530

    question schema change  On branch master  Changes to be committed: 	modified:   app/models/questions.js

--- app/models/questions.js
+++ app/models/questions.js
@@ -1,13 +1,27 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// Sub Document Rated
+
+const RatedBySchema = new Schema({
+  username: {
+    type: String,
+  },
+  rating: {
+    type: Number
+  },
+});
+
 const Questions = new Schema({
-  urls: [
-    {
-      type: String,
-    }
-  ],
+  cf_id: {
+    type: String,
+    unique: true,
+  },
   mean: Number,
+  rated_by: {
+    type: [RatedBySchema],
+    default: []
+  },
   median: Number,
   mode: Number,
 }, { timestamps: true });
